34a8ecb5271d0185d9b6ef0db34e04c88e3db59b
VarianWrynn <yuna1111@163.com>
2020-04-07 12:58:16 +0800

    get previous method

--- SSMiniProgram/Controllers/ClassicController.cs
+++ SSMiniProgram/Controllers/ClassicController.cs
@@ -17,18 +17,35 @@ namespace SSMiniProgram.Controllers
 
 
         [HttpGet]
-        public Lates Get()
+        public Lates Get(int id)
         {
+            if (id > 0)
+            {
+                return new Lates
+                {
+                    content = "You are my Shining Star",
+                    fav_nums = 118,
+                    id = 2,
+                    image = "https://www.tmclee.com/Lee/images/3.jpg",
+                    index = 3,
+                    like_status = 0,
+                    pubdate = "2020-02-22",
+                    title = "Shining Stars",
+                    type = 100
+                };
+
+            }
+
             return new Lates
             {
                 content = "浩瀚群星，璀璨如你",
                 fav_nums = 100,
                 id = 1,
                 image = "https://www.tmclee.com/Lee/images/img_bg_1.jpg",
-                index = 7,
+                index = 2,
                 like_status = 0,
                 pubdate = "2020-02-22",
-                title = "群星璀璨",
+                title = "Shining Stars",
                 type = 100
             };
 
