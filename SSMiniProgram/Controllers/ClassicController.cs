75db8eda5fa089d73b60bb89ab077c8c7b9dfed1
VarianWrynn <yuna1111@163.com>
2020-04-07 20:22:44 +0800

    webApi

--- SSMiniProgram/Controllers/ClassicController.cs
+++ SSMiniProgram/Controllers/ClassicController.cs
@@ -17,25 +17,8 @@ namespace SSMiniProgram.Controllers
 
 
         [HttpGet]
-        public Lates Get(int id)
+        public Lates Get()
         {
-            if (id > 0)
-            {
-                return new Lates
-                {
-                    content = "You are my Shining Star",
-                    fav_nums = 118,
-                    id = 2,
-                    image = "https://www.tmclee.com/Lee/images/3.jpg",
-                    index = 3,
-                    like_status = 0,
-                    pubdate = "2020-02-22",
-                    title = "Shining Stars",
-                    type = 100
-                };
-
-            }
-
             return new Lates
             {
                 content = "浩瀚群星，璀璨如你",
@@ -49,6 +32,53 @@ namespace SSMiniProgram.Controllers
                 type = 100
             };
 
+        }
+
+
+        /// <summary>
+        /// 前端用这种方式获取到 https://localhost:5001/Classic/2/Previous
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        [HttpGet(template:"{index}/previous",Name = "GetByindex")]
+        public Lates GetByindex(int index)
+        {
+
+            return new Lates
+            {
+                content = "相约头马，群星璀璨",
+                fav_nums = 118,
+                id = index,
+                image = "https://www.tmclee.com/Lee/images/3.jpg",
+                index = 3,
+                like_status = 0,
+                pubdate = "2020-02-22",
+                title = "You are my Shining Star",
+                type = 100
+            };
+        }
+
+        /// <summary>
+        /// https://localhost:5001/Classic/GetById2?id=2 这种方式可以取到
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("GetById2")]
+        public Lates GetById2(int id)
+        {
+
+            return new Lates
+            {
+                content = "Leeeeeeeeee",
+                fav_nums = 118,
+                id = id,
+                image = "https://www.tmclee.com/Lee/images/3.jpg",
+                index = 3,
+                like_status = 0,
+                pubdate = "2020-02-22",
+                title = "Shining Stars",
+                type = 100
+            };
         }
     }
 }
