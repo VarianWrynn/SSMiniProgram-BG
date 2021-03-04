e8ac02aed853e86ac23593034fd487172946f598
VarianWrynn <yuna1111@163.com>
2021-03-04 23:42:24 +0800

    book detail repository

---
+++ DAL/Interface/IBook_DetailRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model.POCOs;
+
+namespace DAL.Interface
+{
+    public interface IBook_DetailRepository : IBaseRepository<book_detail>
+    {
+    }
+}
---
+++ DAL/Repository/Book_DetailRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Interface;
+using Model.POCOs;
+
+namespace DAL.Repository
+{
+    public class Book_DetailRepository : BaseRepository<book_detail>, IBook_DetailRepository
+    {
+        public Book_DetailRepository(DBContext context) : base(context)
+        {
+        }
+    }
+
+}
