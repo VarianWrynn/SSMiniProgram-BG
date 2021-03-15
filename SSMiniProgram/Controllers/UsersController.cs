3b6a949ee98027e873eeb082b5e2338d14ac5b51
Lee.Wang <yuna1111@163.com>
2021-03-15 17:39:34 +0800

    Auheorize

---
+++ SSMiniProgram/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSMiniProgram.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : Controller
+    {
+
+        private IUserService _userService;
+        private IMapper _mapper;
+        //private readonly AppSettings _appSettings;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
